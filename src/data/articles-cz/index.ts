770e72b3b444b001f9c08b433f4d8007afb4a62e
Макс <icq416597405@gmail.com>
2026-05-06 00:37:51 +0500

    site: restore content zavod article exports

---
+++ src/data/articles-cz/index.ts
@@ -0,0 +1,21 @@
+// Автоматически сгенерировано Контент Заводом. Не редактировать вручную.
+// Подключение в src/data/articles.ts: `import { czArticles } from './articles-cz'; ... ...czArticles`
+import type { Article } from '../articles';
+
+import { bolotohodIliSmertPochemuAprelUbivaetTehnikuSilneeChemYanvarArticle } from './bolotohod-ili-smert-pochemu-aprel-ubivaet-tehniku-silnee-chem-yanvar';
+import { chtoEsliVashBolotohodZastryalNochyuVGlushiRealnayaIstoriyaSpaseniyaIVyvodyArticle } from './chto-esli-vash-bolotohod-zastryal-nochyu-v-glushi-realnaya-istoriya-spaseniya-i-vyvody';
+import { dveNedeliNaKolesahDnevnikMotoputeshestviyaPoYuzhnomuUraluVesnoyArticle } from './dve-nedeli-na-kolesah-dnevnik-motoputeshestviya-po-yuzhnomu-uralu-vesnoy';
+import { gryazevoyTurizm2026GaydPoLuchshimMarshrutamTsentralnoyRossiiINeTolkoDlyaKvadrotsiklovIBolotohodovArticle } from './gryazevoy-turizm-2026-gayd-po-luchshim-marshrutam-tsentralnoy-rossii-i-ne-tolko-dlya-kvadrotsiklov-i-bolotohodov';
+import { masloFiltryRezinaChtoRealnoNuzhnoMenyatKazhduyuVesnuAChtoMarketingArticle } from './maslo-filtry-rezina-chto-realno-nuzhno-menyat-kazhduyu-vesnu-a-chto-marketing';
+import { top5NeochevidnyhProblemSKotorymiStalkivayutsyaVladeltsyKvadrotsiklovVesnoyIKakIhIzbezhatArticle } from './top-5-neochevidnyh-problem-s-kotorymi-stalkivayutsya-vladeltsy-kvadrotsiklov-vesnoy-i-kak-ih-izbezhat';
+import { vesenniyTyuning7ByudzhetnyhApgreydovKotoryePreobrazyatVashKvadrotsiklKLetuArticle } from './vesenniy-tyuning-7-byudzhetnyh-apgreydov-kotorye-preobrazyat-vash-kvadrotsikl-k-letu';
+
+export const czArticles: Article[] = [
+	bolotohodIliSmertPochemuAprelUbivaetTehnikuSilneeChemYanvarArticle,
+	chtoEsliVashBolotohodZastryalNochyuVGlushiRealnayaIstoriyaSpaseniyaIVyvodyArticle,
+	dveNedeliNaKolesahDnevnikMotoputeshestviyaPoYuzhnomuUraluVesnoyArticle,
+	gryazevoyTurizm2026GaydPoLuchshimMarshrutamTsentralnoyRossiiINeTolkoDlyaKvadrotsiklovIBolotohodovArticle,
+	masloFiltryRezinaChtoRealnoNuzhnoMenyatKazhduyuVesnuAChtoMarketingArticle,
+	top5NeochevidnyhProblemSKotorymiStalkivayutsyaVladeltsyKvadrotsiklovVesnoyIKakIhIzbezhatArticle,
+	vesenniyTyuning7ByudzhetnyhApgreydovKotoryePreobrazyatVashKvadrotsiklKLetuArticle,
+];
