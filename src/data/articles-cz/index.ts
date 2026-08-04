163e6f44eb4bb9ff54803bf62d00773e33140e3e
Макс <icq416597405@gmail.com>
2026-08-04 20:36:07 +0500

    content: sync Rosomaha water-stop article

--- src/data/articles-cz/index.ts
+++ src/data/articles-cz/index.ts
@@ -5,6 +5,7 @@ import type { Article } from '../articles';
 import { bolotohodIliSmertPochemuAprelUbivaetTehnikuSilneeChemYanvarArticle } from './bolotohod-ili-smert-pochemu-aprel-ubivaet-tehniku-silnee-chem-yanvar';
 import { chtoEsliVashBolotohodZastryalNochyuVGlushiRealnayaIstoriyaSpaseniyaIVyvodyArticle } from './chto-esli-vash-bolotohod-zastryal-nochyu-v-glushi-realnaya-istoriya-spaseniya-i-vyvody';
 import { dveNedeliNaKolesahDnevnikMotoputeshestviyaPoYuzhnomuUraluVesnoyArticle } from './dve-nedeli-na-kolesah-dnevnik-motoputeshestviya-po-yuzhnomu-uralu-vesnoy';
+import { esliRosomahaZaglohlaVVodeBezopasnyyPoryadokDeystviyNaMarshruteArticle } from './esli-rosomaha-zaglohla-v-vode-bezopasnyy-poryadok-deystviy-na-marshrute';
 import { gryazevoyTurizm2026GaydPoLuchshimMarshrutamTsentralnoyRossiiINeTolkoDlyaKvadrotsiklovIBolotohodovArticle } from './gryazevoy-turizm-2026-gayd-po-luchshim-marshrutam-tsentralnoy-rossii-i-ne-tolko-dlya-kvadrotsiklov-i-bolotohodov';
 import { masloFiltryRezinaChtoRealnoNuzhnoMenyatKazhduyuVesnuAChtoMarketingArticle } from './maslo-filtry-rezina-chto-realno-nuzhno-menyat-kazhduyu-vesnu-a-chto-marketing';
 import { top5NeochevidnyhProblemSKotorymiStalkivayutsyaVladeltsyKvadrotsiklovVesnoyIKakIhIzbezhatArticle } from './top-5-neochevidnyh-problem-s-kotorymi-stalkivayutsya-vladeltsy-kvadrotsiklov-vesnoy-i-kak-ih-izbezhat';
@@ -14,6 +15,7 @@ export const czArticles: Article[] = [
 	bolotohodIliSmertPochemuAprelUbivaetTehnikuSilneeChemYanvarArticle,
 	chtoEsliVashBolotohodZastryalNochyuVGlushiRealnayaIstoriyaSpaseniyaIVyvodyArticle,
 	dveNedeliNaKolesahDnevnikMotoputeshestviyaPoYuzhnomuUraluVesnoyArticle,
+	esliRosomahaZaglohlaVVodeBezopasnyyPoryadokDeystviyNaMarshruteArticle,
 	gryazevoyTurizm2026GaydPoLuchshimMarshrutamTsentralnoyRossiiINeTolkoDlyaKvadrotsiklovIBolotohodovArticle,
 	masloFiltryRezinaChtoRealnoNuzhnoMenyatKazhduyuVesnuAChtoMarketingArticle,
 	top5NeochevidnyhProblemSKotorymiStalkivayutsyaVladeltsyKvadrotsiklovVesnoyIKakIhIzbezhatArticle,
